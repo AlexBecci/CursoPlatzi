3db3a9a4244fa401a5425dd55b18bedc17923c96
Alex <beccialex2002@gmail.com>
2023-04-26 19:55:47 -0300

    Variables

--- Curso basico de Javascript/variables.js
+++ Curso basico de Javascript/variables.js
@@ -1,1 +1,17 @@
-var nombre = "Lionel"
+var nombre = "Lionel";
+//declarar variable
+
+var edad;
+//Inicializando la variable
+
+edad = 20;
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+  nombre: "Alex",
+  edad: 20,
+  ciudad: "buenos aires",
+};
+
+persona;

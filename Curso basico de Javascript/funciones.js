c23f5dcf6f6e0f7cfcd190430467539e27bf5468
Alex <beccialex2002@gmail.com>
2023-04-26 20:06:24 -0300

    funciones

---
+++ Curso basico de Javascript/funciones.js
@@ -0,0 +1,25 @@
+//Declarativas
+function miFuncion() {
+  return 4;
+}
+
+miFuncion();
+
+//Expresion
+
+var miFuncion = function () {
+  return a + b;
+};
+
+miFuncion();
+
+//Ejemplo
+function sumar(a,b){
+    var res = a+ b;
+
+    return resultado;
+}
+
+sumar(5,10);
+//sumar =15
+

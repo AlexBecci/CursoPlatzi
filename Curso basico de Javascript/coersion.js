bfaf106b7f53b3fcbc2e4830806ec312e0824cac
Alex <beccialex2002@gmail.com>
2023-04-27 12:15:29 -0300

    truthyFalsy

---
+++ Curso basico de Javascript/coersion.js
@@ -0,0 +1,16 @@
+var a = 4 + "7"; //47
+var b = 4 * "7"; //28
+
+typeof a; //String
+typeof b; //number
+
+var c = 20;
+
+var d = c + "";
+console.log(d); //String
+
+var e = String(c);
+typeof e; //string
+
+var f = Number(e);
+typeof f; //Number

a6f4730e1c54a86ad38b2d76270b534b48a586a6
Alex <beccialex2002@gmail.com>
2023-04-27 12:21:50 -0300

    operadores

---
+++ Curso basico de Javascript/operadores.js
@@ -0,0 +1,37 @@
+3 + 2;
+
+50 - 10;
+
+10 * 20;
+
+20 / 2;
+
+"Alex " + "Becci"; // Alex Becci
+
+!false; //true
+!true; //false
+
+//Asignacion
+
+var a = 1; // =
+
+//comparar
+3 == "3"; //true
+3 === "3"; //false por el tipo de dato
+2 == 3; // false
+
+5 < 3; //false menor<
+5 > 3; // true mayor>
+
+5 <= 6; //true
+5 >= 6; //fasle
+
+a && b; //para que se cumpla la condicion las dos tienen que ser verdad
+
+a || b; //con que una sea verdad es true
+
+var edad = 40;
+
+edad++; //40+1
+
+edad += 2; //41+2
